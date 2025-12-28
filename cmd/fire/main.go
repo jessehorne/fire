5d8f8a3aebe9ce5d047cde84abccf54e2ce19b07
Jesse H <j.horne2796@gmail.com>
2025-12-27 20:39:43 -0800

    added terrible lighting

--- cmd/fire/main.go
+++ cmd/fire/main.go
@@ -12,14 +12,30 @@ func main() {
 	rl.SetTargetFPS(60)
 
 	g := internal.NewGame()
+	lightMask := rl.LoadRenderTexture(int32(rl.GetScreenWidth()), int32(rl.GetScreenHeight()))
 
 	for !rl.WindowShouldClose() {
+		// lighting stuff
+		rl.BeginTextureMode(lightMask)
+		rl.ClearBackground(rl.Blank)
+		rl.DrawRectangle(0, 0, int32(rl.GetScreenWidth()), int32(rl.GetScreenHeight()), rl.NewColor(0, 0, 0, 240))
+		rl.BeginBlendMode(rl.BlendAlpha)
+		firePos := rl.GetWorldToScreen2D(rl.NewVector2(0, 0), g.Camera)
+		rl.DrawCircleGradient(int32(firePos.X), int32(firePos.Y-16), 255*g.Camera.Zoom, rl.NewColor(255, 255, 255, 50), rl.Blank)
+		rl.EndBlendMode()
+		rl.EndTextureMode()
 
+		// normal drawing stuff
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.Black)
 
 		g.Update()
 		g.Draw()
+
+		// draw light mask
+		s := rl.NewRectangle(0, 0, float32(lightMask.Texture.Width), float32(-lightMask.Texture.Height))
+		d := rl.NewRectangle(0, 0, float32(rl.GetScreenWidth()), float32(rl.GetScreenHeight()))
+		rl.DrawTexturePro(lightMask.Texture, s, d, rl.NewVector2(0, 0), 0.0, rl.White)
 
 		rl.EndDrawing()
 	}
